e3395099caacbecd44f731e453ec9221b9cfa051
Wagner Bonfim <wagner.bfm@gmail.com>
2020-03-11 16:35:58 -0300

    Adição de Histórico de Operações

--- Conta.java
+++ Conta.java
@@ -1,4 +1,6 @@
-package iniciante.banco;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Wagner Bonfim <github.com/wagfim>
  */
@@ -8,6 +10,7 @@ public class Conta {
     private double saldo;
     private Data dataAbertura;
     private Cliente cliente;
+    List<Operacao> historicoOperacoes;
 
     public Conta(int numero, int senha, Data dataAbertura, double saldo, Cliente cliente) {
         this.numero = numero;
@@ -15,6 +18,7 @@ public class Conta {
         this.dataAbertura = dataAbertura;
         this.saldo = saldo;
         this.cliente = cliente;
+        this.historicoOperacoes = new ArrayList();
     }
     
     public Conta(int numero, int senha, Data dataAbertura, Cliente cliente) {
@@ -25,15 +29,17 @@ public class Conta {
         if (saldo > 0) {
             if (valor > 0 && valor <= saldo) {
                 saldo -= valor;
+                armazenaDadosOperacao("Saque", this.numero, valor);
                 return true; //sucesso
             }
         }
         return false;
-    }
+    }  
     
     public boolean  depositar(double valor) {
         if (valor > 0) {
             saldo += valor;
+            armazenaDadosOperacao("Deposito", this.numero, valor);
             return true;
         }
         return false;
@@ -42,9 +48,14 @@ public class Conta {
     public boolean transferirValor(double valor, Conta contaDestino) {
         if (!this.equals(contaDestino) && sacar(valor)) {
             contaDestino.depositar(valor);
+            armazenaDadosOperacao("Transferencia", contaDestino.getNumero(), valor);
             return true;
         }
         return false;
+    }
+    
+    public void armazenaDadosOperacao(String nome, int numBeneficiario, double valor) {
+        historicoOperacoes.add(new Operacao(nome, numBeneficiario, valor));
     }
     
     public void imprimeInformacoesConta() {
@@ -56,6 +67,33 @@ public class Conta {
         System.out.println("*****************************************");
     }
     
+    public void imprimeExtrato() {
+        System.out.println("Operacao\tBeneficiario\tValor ");
+        for(Operacao operacao : historicoOperacoes) {
+            System.out.print(operacao.getNomeOperacao());
+            if(operacao.getNomeOperacao().equals("Saque"))
+                System.out.print("...........");
+            if(operacao.getNomeOperacao().equals("Deposito"))
+                System.out.print("........");
+            if(operacao.getNomeOperacao().equals("Transferencia"))
+                System.out.print("...");
+            System.out.print(operacao.getNumBeneficiario() + "............");
+            System.out.println(operacao.getValor());;  
+        }
+        /* Espaçamento usando tabs
+        for(Operacao operacao : historicoOperacoes) {
+            System.out.print(operacao.getNomeOperacao());
+            if(operacao.getNomeOperacao().equals("Saque"))
+                System.out.print("\t\t");
+            if(operacao.getNomeOperacao().equals("Deposito"))
+                System.out.print("\t");
+            if(operacao.getNomeOperacao().equals("Transferencia"))
+                System.out.print("\t");
+            System.out.print(operacao.getNumBeneficiario() + "\t");
+            System.out.println(operacao.getValor());;  
+        }
+        */     
+    }
 
     public int getNumero() {
         return numero;
@@ -84,7 +122,4 @@ public class Conta {
     public Cliente getCliente() {
         return cliente;
     }
-    
-    
-    
 }
