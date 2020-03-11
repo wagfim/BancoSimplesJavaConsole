0fd7e2993969c72c962998b877db6a972ccf32c2
Wagner Bonfim <wagner.bfm@gmail.com>
2020-03-11 14:55:13 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,191 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+/**
+ * @author Wagner Bonfim <github.com/wagfim>
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int numeroConta;
+        int senhaInformada;
+        boolean sairDoSistema = false;
+        
+        ClientesProntos clientes = new ClientesProntos();
+        List<Conta> listaContas = new ArrayList<>();
+        
+        Conta conta1 = new Conta(4758, 12345, new Data(10,3,2020), 87.00, clientes.cliente1);
+        Conta conta2 = new Conta(2045, 12345, new Data(10,3,2020), 758.80, clientes.cliente2);
+        Conta conta3 = new Conta(1584, 12345, new Data(10,3,2020), 10000.00, clientes.cliente3);   
+        listaContas.add(conta1);
+        listaContas.add(conta2);
+        listaContas.add(conta3);
+        
+        System.out.println("************************");
+        System.out.println("   Bem vindo ao Banco   ");
+        System.out.println("************************");
+        System.out.println();
+        
+        do {
+            //identifica a conta do cliente
+            do {            
+            System.out.print("\nInforme o seu numero de conta: ");
+            numeroConta = Integer.parseInt(scan.next());
+            
+            if (!checaConta(numeroConta, listaContas))
+                System.out.println("NÃºmero invÃ¡lido. Tente novamente");
+            
+            } while (!checaConta(numeroConta, listaContas));
+
+            Conta contaEscolhida = retornaConta(numeroConta, listaContas);
+
+            do {
+                System.out.print("Informe a sua senha: ");
+                senhaInformada = Integer.parseInt(scan.next());
+
+                if (!checaSenha(senhaInformada, contaEscolhida))
+                    System.out.println("Senha invÃ¡lido. Tente novamente");
+
+            } while (!checaSenha(senhaInformada, contaEscolhida));
+            //fim identifica conta
+            
+            //menu de conta
+            if(!Objects.isNull(contaEscolhida)) {
+                boolean continueRodando = true;
+                int opcaoEscolhida;
+
+                System.out.println("\nOlÃ¡, " + contaEscolhida.getCliente().getNome() + "!");
+
+                do {                
+                    System.out.println("\nQual operaÃ§Ã£o deseja realizar?");
+                    System.out.println("1. Saldo");
+                    System.out.println("2. Saque");
+                    System.out.println("3. DepÃ³sito");
+                    System.out.println("4. Transferencia entre contas");
+                    System.out.println("5. InformaÃ§Ãµes desta conta");
+                    System.out.println("6. Alternar para outra conta");
+                    System.out.println("0. Sair");
+                    System.out.print("> ");
+
+                    opcaoEscolhida = Integer.parseInt(scan.next());
+
+                    switch(opcaoEscolhida) {
+                        case 0:
+                            continueRodando = false; //sai do loop do menu de conta
+                            sairDoSistema = true; //sai do loop que mantem o sistema
+                        break;
+                        case 1:
+                            System.out.println("Saldo: " + contaEscolhida.getSaldo());
+                            pressioneEnter();
+                        break;
+                        case 2:
+                            executaProcessoDeSaque(listaContas, contaEscolhida);
+                            pressioneEnter();
+                        break;
+                        case 3:
+                            executaProcessoDeDeposito(listaContas, contaEscolhida);
+                            pressioneEnter();
+                        break;
+                        case 4:
+                            executaProcessoDeTransferencia(listaContas, contaEscolhida);
+                            pressioneEnter();
+                        break;
+                        case 5:
+                            contaEscolhida.imprimeInformacoesConta();
+                            pressioneEnter();
+                        break;
+                        case 6:
+                            continueRodando = false;
+                            pressioneEnter();
+                        break;
+                        default:
+                            System.out.println("OpÃ§Ã£o invÃ¡lida");
+                        break;
+                    }
+                } while (continueRodando);
+            } //fim menu de conta //
+        } while (!sairDoSistema); //fim sistema
+    }
+    
+    public static void executaProcessoDeSaque(List<Conta> listaContas, Conta conta) {
+        Scanner scan = new Scanner(System.in);
+        double valor;
+
+        System.out.print("Valor: ");
+        valor = scan.nextDouble();
+        if (conta.sacar(valor)) {
+            System.out.println("OperaÃ§Ã£o realizada com sucesso!");
+        } else {
+            System.out.println("Erro na operacao");
+        }
+    }
+    
+    public static void executaProcessoDeDeposito(List<Conta> listaContas, Conta conta) {
+        Scanner scan = new Scanner(System.in);
+        double valor;
+        
+        System.out.print("Valor: ");
+        valor = scan.nextDouble();
+        if (conta.depositar(valor)) {
+            System.out.println("OperaÃ§Ã£o realizada com sucesso!");
+        } else {
+            System.out.println("Erro na operacao");
+        }
+    }
+    
+    public static void executaProcessoDeTransferencia(List<Conta> listaContas, Conta contaOrigem) {
+        Scanner scan = new Scanner(System.in);
+        double valor;
+        
+        int numeroContaDestino;   
+        do {
+            System.out.print("Informe o nÃºmero da conta destino: ");
+            numeroContaDestino = Integer.parseInt(scan.nextLine());
+            if (!checaConta(numeroContaDestino, listaContas)) {
+                System.out.println("NÃºmero incorreto. Verifique e tente novamente.");
+            }
+        } while (!checaConta(numeroContaDestino, listaContas));
+
+        Conta contaDestino = retornaConta(numeroContaDestino, listaContas);
+
+        System.out.print("Valor: ");
+        valor = scan.nextDouble();
+
+        if (contaOrigem.transferirValor(valor, contaDestino)) {
+            System.out.println("OperaÃ§Ã£o realizada com sucesso!");
+        } else {
+            System.out.println("Erro na operacao");
+        }
+    }
+    
+    public static Conta retornaConta(int numero, List<Conta> listaContas) {
+        for (int i = 0; i < listaContas.size(); i++) {
+            if (listaContas.get(i).getNumero() == numero) {
+                return listaContas.get(i);
+            }
+        }
+        return null;
+    }
+    public static boolean checaSenha(int senha, Conta conta) {
+        if (conta.getSenha() == senha) {
+            return true;
+        }
+        return false;
+    }
+    public static boolean checaConta(int numeroConta, List<Conta> listaContas) {
+        for (int i = 0; i < listaContas.size(); i++) {
+            if (listaContas.get(i).getNumero() == numeroConta) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void pressioneEnter() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Pressione Enter para continuar...");
+        scan.nextLine();
+        //scan.close();
+    }
+    
+}
